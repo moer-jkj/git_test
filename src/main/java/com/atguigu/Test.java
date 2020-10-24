036298083bdf11d50738056af3cbb3635ebcf9e1
moer-jkj <Moermanske@163.com>
2020-10-24 16:57:06 +0800

    3333

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -2,7 +2,6 @@ package com.atguigu;
 
 public class Test {
     public static void main(String[] args) {
-
         System.out.println("1111 = " + 1111);
         System.out.println("1111 = " + 2222);
 
